b9a40ac1c6eab5c80b5391084c2fcd31e7023d1c
unknown <muktinathrajbanshi786@gmail.com>
2026-01-09 16:44:02 +0545

    wow images and array of an object is created

---
+++ script.js
@@ -0,0 +1,26 @@
+let cardsArray = [
+    {
+        "name": "CSS",
+        "img": "./images/css.png",
+    },
+     {
+        "name": "HTML",
+        "img": "./images/html.png",
+    },
+     {
+        "name": "jQuery",
+        "img": "./images/jquery.png",
+    },
+     {
+        "name": "JS",
+        "img": "./images/js.png",
+    },
+     {
+        "name": "Node",
+        "img": "./images/node.png",
+    },
+     {
+        "name": "Python",
+        "img": "./images/python.png",
+    }
+]
