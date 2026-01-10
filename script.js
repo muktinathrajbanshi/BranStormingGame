1b6d88aa1becf969bf01c120e6e834d3fa126172
unknown <muktinathrajbanshi786@gmail.com>
2026-01-10 10:55:34 +0545

    wow with limited code the shuffled functionality is done

--- script.js
+++ script.js
@@ -27,11 +27,34 @@ let cardsArray = [
 
 const parentDiv = document.querySelector("#card-section");
 
-for (let i=0; i<cardsArray.length; i++) {
+// step 2: to duplicate each card
+const gameCard = cardsArray.concat(cardsArray)
+console.log(gameCard);
+
+// step 3: Note that this method creates a new shuffled array instead of modifying the original one.
+
+// const myNumbers = (array) => {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor( Math.random()* (i + 1))
+//         console.log(i, j);
+
+//         let temp = array[i]
+//         array[i] = array[j]
+//         array[j] = temp        
+//     }
+//     return array;
+// };
+
+// const shuffledChild = myNumbers(gameCard)
+
+let shuffledChild = Array.from(gameCard).sort(() => .5 - Math.random());
+
+// step 1 to add the card 
+for (let i=0; i<shuffledChild.length; i++) {
     const childDiv = document.createElement("div")
     childDiv.classList.add("card")
-    childDiv.dataset.name = cardsArray[i].name;
-    childDiv.style.backgroundImage = `url(${cardsArray[i].img})`;
+    childDiv.dataset.name = shuffledChild[i].name;
+    childDiv.style.backgroundImage = `url(${shuffledChild[i].img})`;
     parentDiv.appendChild(childDiv)
 
 
